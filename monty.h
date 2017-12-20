826c9eca2fe1e21386ca988b7567fc2e453a4e7e
magezil <256@holbertonschool.com>
2017-12-20 02:52:51 +0000

    add sub

--- monty.h
+++ monty.h
@@ -44,8 +44,9 @@ void pint(stack_t **stack, unsigned int line_number);
 int pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 
-void add(stack_t **stack, unsigned int line_number);
-
 void nop(stack_t **stack, unsigned int line_number);
+
+void add(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
 
 #endif
