59502cea2d3dcf334df12cba848bd645ef216de0
magezil <256@holbertonschool.com>
2017-12-20 19:47:11 +0000

    add, sub, mul, div, mod opcodes

--- monty_math.c
+++ monty_math.c
@@ -16,12 +16,12 @@ void add(stack_t **stack, unsigned int line_number)
 	}
 
 	top = pop(stack, line_number);
-	value = top + pop(stack, line_number);
+	value = pop(stack, line_number) + top;
 	push(stack, line_number);
 }
 
 /**
- * sub - adds the top two elements of the stack
+ * sub - subtracts the top element from the second to the top element of the stack
  * @stack: top of the stack
  * @line_number: current line number
  */
@@ -37,5 +37,76 @@ void sub(stack_t **stack, unsigned int line_number)
 
 	top = pop(stack, line_number);
 	value = pop(stack, line_number) - top;
+	push(stack, line_number);
+}
+
+/**
+ * div - divides the top element from the second to the top element of the stack
+ * @stack: top of the stack
+ * @line_number: current line number
+ */
+void div(stack_t **stack, unsigned int line_number)
+{
+	int top;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		printf("L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (value == 0)
+	{
+		printf("L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	top = pop(stack, line_number);
+	value = pop(stack, line_number) / top;
+	push(stack, line_number);
+}
+
+/**
+ * mul - multiplies the top two elements of the stack
+ * @stack: top of the stack
+ * @line_number: current line number
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+	int top;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		printf("L%d: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	
+	top = pop(stack, line_number);
+	value = pop(stack, line_number) * top;
+	push(stack, line_number);
+}
+
+/**
+ * mod - computes the remainder of the division of the top element
+ *       from the second to the top element of the stack
+ * @stack: top of the stack
+ * @line_number: current line number
+ */
+void mod(stack_t **stack, unsigned int line_number)
+{
+	int top;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		printf("L%d: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (value == 0)
+	{
+		printf("L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	top = pop(stack, line_number);
+	value = pop(stack, line_number) % top;
 	push(stack, line_number);
 }
